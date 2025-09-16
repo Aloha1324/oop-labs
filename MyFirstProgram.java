293582632e69771de0172f3b04e8744564911216
Aloha1324 <sergey.dudkin.2005@mail.ru>
2025-09-16 15:47:51 +0400

    Task1

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,7 +1,5 @@
 class MyFirstClass {
     public static void main(String[] s) {
-        for (int i = 0; i < s.length; i++) {
-            System.out.println(s[i]);
-        }
+        System.out.println("Hello world!!!");
     }
 }
