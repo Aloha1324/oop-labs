491fc294ef8c56a2a2163355b4ba31ff7a1e2d1a
Aloha1324 <sergey.dudkin.2005@mail.ru>
2025-09-16 15:51:58 +0400

    Add files via upload

---
+++ L1_OOP/Task4/MyFirstProgram.java
@@ -0,0 +1,20 @@
+import myfirstpackage.*;
+
+class MyFirstClass {
+	public static void main(String[] s) {
+		MyFirstPackage o = new MyFirstPackage (1, 1);
+		System.out.println(o.integerDivision());
+		for (int i = 1; i <= 8; i++) {
+			for (int j = 1; j <= 8; j++) {
+				o.setA(i);
+				o.setB(j);
+				System.out.print(o.integerDivision());
+				System.out.print(" ");
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
